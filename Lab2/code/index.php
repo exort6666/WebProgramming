1070dfffbdbf5c2545e9eec6af76ff0920aff8f8
exort6666 <evgeniylagunv@mail.ru>
2024-03-20 20:19:54 +0200

    Complete Task 2 - Numbers.

--- Lab2/code/index.php
+++ Lab2/code/index.php
@@ -1,15 +1,33 @@
 <?php
-/* Task 1. Доступ по ссылке
-    Вы унаследовали код от другого разработчика. Вы не можете изменить их код,
-но вам необходимо добавить некоторые дополнительные функции. Вместо использования
-переменной $very_bad_unclear_name, как в вашей части кода. Объявите новую переменную
-$order как ссылку на переменную $very_bad_unclear_name.
-    С помощью оператора конкатенации добавьте к переменной $order любую строку.
-    Распечатайте $very_bad_unclear_name */
+// Task 1. Доступ по ссылке.
+
 $very_bad_unclear_name = "15 chicken wings";
+
 // Write your code here:
 $order = &$very_bad_unclear_name;
 $order .= 'anyString';
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+echo "\n\n";
+
+//Task 2. Числа.
+$varInt = 228;
+echo "int: $varInt";
+echo "\n";
+
+$varFloat = 3.22;
+echo "float: $varFloat \n";
+
+$var12 = 6*2; //Не понятен комментарий к этому пункту
+echo "$var12 \n";
+
+$lastMonth = 1187.23;
+$this_month = 1089.98;
+echo $lastMonth-$this_month;
+echo "\n";
+
+
+
+
